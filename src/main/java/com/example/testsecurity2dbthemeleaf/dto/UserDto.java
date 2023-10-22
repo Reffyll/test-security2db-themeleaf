9ea4f8c409aafd89e858391a510a9e9cf6332514
reffyll <rdm008@yandex.ru>
2023-10-22 18:22:24 +0500

    lr7

--- src/main/java/com/example/testsecurity2dbthemeleaf/dto/UserDto.java
+++ src/main/java/com/example/testsecurity2dbthemeleaf/dto/UserDto.java
@@ -1,2 +1,31 @@
-package com.example.testsecurity2dbthemeleaf.dto;public class UserDto {
+package com.example.testsecurity2dbthemeleaf.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private Long id;
+    @NotEmpty
+    private String firtsName;
+
+    @NotEmpty
+    private String lastName;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email
+    private String email;
+
+    @NotEmpty(message = "Password should not be empty")
+    private String password;
+
 }
